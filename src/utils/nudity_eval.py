719974ad4bf75986ef8f5f8646a056d4c14cead0
Agnieszka <146332030+AgnieszkaPolowczyk@users.noreply.github.com>
2025-08-18 13:48:08 +0200

    Revert "usuniecie komentarza"

--- src/utils/nudity_eval.py
+++ src/utils/nudity_eval.py
@@ -28,9 +28,8 @@ def nudent_classify(folder, out_file="./nudenet/results_nudenet.txt"):
     detected_classes = dict.fromkeys(detector_v2_default_classes, 0)
     file_list, detect_list = [], []
 
-    #nudity detection on every image
     for image_file in tqdm.tqdm(image_files):
-        detector = NudeDetector() 
+        detector = NudeDetector()  # reinit przy każdym obrazie
         detections = detector.detect(image_file)
         for det in detections:
             if det["class"] in detected_classes:
